b6d1035f736ab41cc9c9c99b7436ed10f63cf7cc
Rodrigo Merlone <alestori7@gmail.com>
2022-05-09 16:12:56 -0300

    prettierrc

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,35 +1,35 @@
-import { PrismaClient } from "@prisma/client";
-import express, { Request, Response } from "express";
-import cors from "cors";
+import { PrismaClient } from '@prisma/client'
+import express, { Request, Response } from 'express'
+import cors from 'cors'
 
-const prisma = new PrismaClient();
-const app = express();
+const prisma = new PrismaClient()
+const app = express()
 
-app.use(express.json());
-app.use(cors());
+app.use(express.json())
+app.use(cors())
 
-app.post("/user", async (req: Request, res: Response) => {
-	const result = await prisma.user.create({
-		data: {
-			...req.body,
-		},
-	});
+app.post('/user', async (req: Request, res: Response) => {
+  const result = await prisma.user.create({
+    data: {
+      ...req.body,
+    },
+  })
 
-	res.json(result);
-});
+  res.json(result)
+})
 
-app.put("/user/:id", async (req: Request, res: Response) => {
-	const { id } = req.params;
-	const { name, email } = req.body;
-	const editedUser = await prisma.user.update({
-		where: { id: Number(id) },
-		data: {
-			name,
-			email,
-		},
-	});
+app.put('/user/:id', async (req: Request, res: Response) => {
+  const { id } = req.params
+  const { name, email } = req.body
+  const editedUser = await prisma.user.update({
+    where: { id: Number(id) },
+    data: {
+      name,
+      email,
+    },
+  })
 
-	res.json(editedUser);
-});
+  res.json(editedUser)
+})
 
-app.listen(3001, () => console.log("Listening on port 3001"));
+app.listen(3001, () => console.log('Listening on port 3001'))
