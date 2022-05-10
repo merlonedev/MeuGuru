db37e6ebeca3f6532ce9b37e43034d0f99ac7cc9
Merlone <alestori7@gmail.com>
2022-05-10 10:53:19 -0300

    form structure

--- frontend/components/RegisterForm.tsx
+++ frontend/components/RegisterForm.tsx
@@ -1,7 +1,30 @@
-export default function RegisterForm () {
+import { Fragment } from 'react'
+import { Button, Form } from 'react-bootstrap'
+
+export default function RegisterForm() {
   return (
-    <form>
+    <Fragment>
       <h3>Cadastrar usuário</h3>
-    </form>
+      <Form className="d-flex flex-column align-items-center">
+        <Form.Group className="mb-3 w-50 p-2" controlId="formBasicEmail">
+          <Form.Label>E-mail</Form.Label>
+          <Form.Control type="email" />
+        </Form.Group>
+
+        <Form.Group className="mb-3 w-50 p-2" controlId="formBasicName">
+          <Form.Label>Nome</Form.Label>
+          <Form.Control type="text" />
+        </Form.Group>
+
+        <Form.Group className="mb-3 w-50 p-2" controlId="formBasicPassword">
+          <Form.Label>Senha</Form.Label>
+          <Form.Control type="password" />
+        </Form.Group>
+
+        <Button variant="primary" type="submit" className="mb-3 w-25">
+          Cadastrar
+        </Button>
+      </Form>
+    </Fragment>
   )
 }
