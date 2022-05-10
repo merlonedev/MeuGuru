044d7296881ba8a4e9ee0e75a2ec08a666d0e2dc
Merlone <alestori7@gmail.com>
2022-05-10 11:13:47 -0300

    successful user register

--- frontend/components/RegisterForm.tsx
+++ frontend/components/RegisterForm.tsx
@@ -1,24 +1,54 @@
-import { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
 import { Button, Form } from 'react-bootstrap'
 
 export default function RegisterForm() {
+  const [info, setInfo] = useState({
+    email: '',
+    name: '',
+    password: '',
+  })
+
+  const onChange = ({ target }) => {
+    setInfo({
+      ...info,
+      [target.id]: target.value,
+    })
+  }
+
+  const onSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault()
+
+    try {
+      await fetch('http://localhost:3001/user', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(info),
+      })
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <Fragment>
       <h3>Cadastrar usuário</h3>
-      <Form className="d-flex flex-column align-items-center">
-        <Form.Group className="mb-3 w-50 p-2" controlId="formBasicEmail">
+      <Form
+        className="d-flex flex-column align-items-center"
+        onSubmit={onSubmit}
+      >
+        <Form.Group className="mb-3 w-50 p-2" controlId="email">
           <Form.Label>E-mail</Form.Label>
-          <Form.Control type="email" />
+          <Form.Control type="email" onChange={(e) => onChange(e)} />
         </Form.Group>
 
-        <Form.Group className="mb-3 w-50 p-2" controlId="formBasicName">
+        <Form.Group className="mb-3 w-50 p-2" controlId="name">
           <Form.Label>Nome</Form.Label>
-          <Form.Control type="text" />
+          <Form.Control type="text" onChange={(e) => onChange(e)} />
         </Form.Group>
 
-        <Form.Group className="mb-3 w-50 p-2" controlId="formBasicPassword">
+        <Form.Group className="mb-3 w-50 p-2" controlId="password">
           <Form.Label>Senha</Form.Label>
-          <Form.Control type="password" />
+          <Form.Control type="password" onChange={(e) => onChange(e)} />
         </Form.Group>
 
         <Button variant="primary" type="submit" className="mb-3 w-25">
